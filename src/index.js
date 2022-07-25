41c5c3f7e1de897ed0f4ed4b659f2d2f1054d09c
0xDA <0xda032@gmail.com>
2022-07-24 22:38:41 -0600

    site skeleton

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Fade from 'react-reveal/Fade';
-import { useTable } from 'react-table';
+import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+import  { VscGithub, VscTwitter, VscMail } from "react-icons/vsc";
 import './index.css';
 
 const root = ReactDOM.createRoot (
@@ -9,11 +10,11 @@ const root = ReactDOM.createRoot (
 );
 
 const name = (
-    <h1 id ="name">0xDA</h1>
+    <h1 id ="name" onClick={scroll.scrollToTop}>0xDA</h1>
 );
 
 function Nav_button(props) {
-    return <button id="nav_button">{props.section}</button>
+    return <button id="nav_button"><Link activeClass="active" smooth offset={-140} spy={true} to={props.section}>{props.section}</Link></button>
 };
 
 const header = (
@@ -22,8 +23,7 @@ const header = (
         <div id="navbar">
             <Nav_button section="projects"/>
             <Nav_button section="gallery"/>
-            <Nav_button section="blog"/>
-            <Nav_button section="socials"/>
+            <Nav_button section="contact"/>
         </div>
     </div>
 );
@@ -32,7 +32,7 @@ function Section(props) {
     return (
         <div class="section">
             <div id={props.section_heading}>
-                <Fade timeout="1000">
+                <Fade timeout={1000}>
                     <SectionHeading section_heading={props.section_heading}/>
                 </Fade>
             {props.content}
@@ -46,11 +46,18 @@ function SectionHeading(props) {
 }
 
 function Project(props) {
+    function openLink() {
+        props.link? window.open(props.link, '_blank') : null;
+    }
     return (
         <div class="project">
-            <div id={props.project_name}>
-                <h3 class="project_name">{props.project_name}</h3>
-                <p class="project_description">{props.description}</p>
+            <div id="indenter">
+                <h3 class="item_name">></h3>
+                <p class="item_description"></p>
+            </div>
+            <div id={props.item_name}>
+                <h3 class="item_name" onClick={openLink} style={props.link? {cursor: "pointer"}: null}>{props.item_name}</h3>
+                <p class="item_description">{props.description}</p>
             </div>
         </div>
     );
@@ -61,10 +68,10 @@ const projects = (
         section_heading="projects"
         content={
             <div class="subsection">
-                <Fade timeout="1000">
-                    <Project project_name="QuantOS" description="revolutionary investment research platform"/>
-                    <Project project_name="lyricle" description="wordle style lyric guessing game"/>
-                    <Project project_name="tbd01" description="an all-in-one accounting solution"/>
+                <Fade timeout={1000}>
+                    <Project item_name="QuantOS" link="https://quantrinsic.com/" description={"modular investment research platform"}/>
+                    <Project item_name="project-02" description="all-in-one accounting solution"/>
+                    <Project item_name="project-03" description="wordle style lyric guessing game"/>
                 </Fade>
             </div>
         } 
@@ -72,12 +79,15 @@ const projects = (
 );
 
 function GalleryItem(props) {
+    function openLink() {
+        props.link? window.open(props.link, '_blank') : null;
+    }
     return (
         <div class="gallery_item">
             <div id={props.item_name}>
                 <img class="media" src={props.source}/>
                 <div>
-                    <h3 class="item_name">{props.item_name}</h3>
+                    <h3 class="item_name" onClick={openLink} style={props.link? {cursor: "pointer"}: null}>{props.item_name}</h3>
                     <p class="item_description">{props.description}</p>
                 </div>
             </div>
@@ -90,23 +100,40 @@ const gallery = (
         section_heading="gallery"
         content={
             <div class="subsection">
-                <Fade timeout="1000">
-                    <GalleryItem source="https://lh3.googleusercontent.com/Noh38d22G2uDIDBvQw_w94Ht9Adh9nUb_SrZcSJq2lIs0ZYbGuEz5nLji_fOjRzBALWtWEx1ePkmw2SuzBU0iECRScZoLgt1BZll=w600" item_name="kanpai panda" description="#8105"/>
+                <Fade timeout={1000}>
+                    <GalleryItem source="https://lh3.googleusercontent.com/Noh38d22G2uDIDBvQw_w94Ht9Adh9nUb_SrZcSJq2lIs0ZYbGuEz5nLji_fOjRzBALWtWEx1ePkmw2SuzBU0iECRScZoLgt1BZll=w600" item_name="kanpai panda" link="https://opensea.io/assets/matic/0xacf63e56fd08970b43401492a02f6f38b6635c91/8105/" description="#8105"/>
                 </Fade>
             </div>
         }
     />
 );
 
-const blog = (
+function Social(props) {
+    function openLink() {
+        props.link? window.open(props.link, '_blank') : null;
+    }
+    return (
+        <div class="social">
+            <div id="social_icon">
+                {props.icon}
+            </div>
+            <div id={props.item_name}>
+                <h3 class="item_name" style={props.link? {cursor: "pointer"}: null} onClick={openLink}>{props.item_name}</h3>
+            </div>
+        </div>
+    );
+}
+const contact = (
     <Section
-        section_heading="blog"
-    />
-);
-
-const socials = (
-    <Section
-        section_heading="socials"
+        section_heading="contact"
+        content={
+            <div class="subsection">
+                <Fade timeout={1000}>
+                    <Social icon={<VscGithub size="2rem"/>} link="https://github.com/0xDA03" item_name="0xDA03"/>
+                    <Social icon={<VscTwitter size="2rem"/>} link="https://twitter.com/0xda03" item_name="0xda03"/>
+                </Fade>
+            </div>
+        }
     />
 );
 
@@ -115,8 +142,7 @@ const app = (
         {header}
         {projects}
         {gallery}
-        {blog}
-        {socials}
+        {contact}
     </div>
 )
 
